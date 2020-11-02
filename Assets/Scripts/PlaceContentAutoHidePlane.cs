0460a222b69fb8172c73bc5d6decca37191718f1
Julinate <julia.renate.schmidt@students.hs-mainz.de>
2020-11-02 17:03:09 +0100

    Plastic Health Risc finished

---
+++ Assets/Scripts/PlaceContentAutoHidePlane.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+
+
+public class PlaceContentAutoHidePlane : MonoBehaviour
+{
+    public ARRaycastManager raycastManager;
+    public GraphicRaycaster raycaster;
+    public ARPlaneManager planeManager;
+    public ARPointCloudManager pointCloudManager;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsClickOverUI())
+        {
+       
+            List<ARRaycastHit> hitPoints = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.mousePosition, hitPoints, TrackableType.Planes);
+
+            if (hitPoints.Count > 0)
+            {
+                Pose pose = hitPoints[0].pose;
+                transform.rotation = pose.rotation;
+                transform.position = pose.position;
+            }
+
+            foreach (var plane in planeManager.trackables)
+            {
+                plane.gameObject.SetActive(false);
+            }
+
+            planeManager.enabled = false;
+
+            foreach (var point in pointCloudManager.trackables)
+            {
+                point.gameObject.SetActive(false);
+            }
+
+            pointCloudManager.enabled = false;
+        }
+    }
+
+    bool IsClickOverUI()
+    {
+        PointerEventData data = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+    };
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(data, results);
+        return results.Count > 0;
+
+    }
+}
